fc82f3690c133e4f72d900d6db42140fa7e75ff0
Christian Nytra <ch.nytra.13@gmail.com>
2024-03-12 12:43:40 +0100

    Pausemenue fertig

---
+++ Scripts/Battle.cs
@@ -0,0 +1,18 @@
+using Godot;
+
+namespace MonkeSurvivor.Scripts;
+
+public partial class Battle : Node
+{
+    [Export]
+    public PauseMenu PauseMenu { get; set; }
+
+    public override void _Process(double delta)
+    {
+        if (Input.IsKeyPressed(Key.Escape))
+        {
+            PauseMenu.Visible = true;
+            GetTree().Paused  = true;
+        }
+    }
+}
