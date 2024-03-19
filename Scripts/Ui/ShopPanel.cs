7d6fcab0e6c7935fd3a98a6705b3a517f4a920c3
Christian Nytra <ch.nytra.13@gmail.com>
2024-03-19 21:05:25 +0100

    irgendwas

--- Scripts/Ui/ShopPanel.cs
+++ Scripts/Ui/ShopPanel.cs
@@ -1,13 +1,10 @@
 using Godot;
-using System;
+
+namespace MonkeSurvivor.Scripts.Ui;
 
 public partial class ShopPanel : PanelContainer
 {
-	public override void _Ready()
-	{
-	}
+    public override void _Ready() { }
 
-	public override void _Process(double delta)
-	{
-	}
+    public override void _Process(double delta) { }
 }
