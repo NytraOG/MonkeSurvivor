1290d36e096e4acc9747d35cb1f5babf4283e9cc
Christian Nytra <ch.nytra.13@gmail.com>
2024-03-26 00:25:56 +0100

    shop shop

--- Scenes/Shop.cs
+++ Scenes/Shop.cs
@@ -1,12 +1,23 @@
 using Godot;
+using MonkeSurvivor.Scripts.Ui;
 
 namespace MonkeSurvivor.Scenes;
 
 public partial class Shop : Node
 {
-    private PackedScene BattleScene => ResourceLoader.Load<PackedScene>("res://Scenes/battle.tscn");
+    private CharacterSheet characterSheet;
+    private Inventory      inventory;
+    private ShopPanel      shopPanel;
+    private PackedScene    BattleScene => ResourceLoader.Load<PackedScene>("res://Scenes/battle.tscn");
 
-    public override void _Ready() => GetTree().Paused = false;
+    public override void _Ready()
+    {
+        GetTree().Paused = false;
+
+        shopPanel      = GetNode<ShopPanel>("%" + nameof(ShopPanel));
+        inventory      = GetNode<Inventory>("%" + nameof(Inventory));
+        characterSheet = GetNode<CharacterSheet>("%" + nameof(CharacterSheet));
+    }
 
     public override void _Process(double delta) { }
 
