8319d2732bdbc87c57aa39dcf14ccb1da430943c
Christian Nytra <ch.nytra.13@gmail.com>
2024-03-25 22:41:27 +0100

    shopcards können verschwinden

--- Scripts/Ui/ShopCard.cs
+++ Scripts/Ui/ShopCard.cs
@@ -4,7 +4,18 @@ namespace MonkeSurvivor.Scripts.Ui;
 
 public partial class ShopCard : PanelContainer
 {
-    public override void _Ready() { }
+    private Label itemCostLabel;
+    private Label itemDescriptionLabel;
+    private Label itemNameLabel;
+
+    public override void _Ready()
+    {
+        itemNameLabel        = GetNode<Label>("%ItemName");
+        itemDescriptionLabel = GetNode<Label>("%ItemDescription");
+        itemCostLabel        = GetNode<Label>("%ItemCost");
+    }
 
     public override void _Process(double delta) { }
+
+    public void _on_buy_pressed() => Modulate = new Color(Modulate, 0);
 }
