f5ed65d8f712d72d523ca9b98b30af6ea553d83c
Christian Nytra <ch.nytra.13@gmail.com>
2024-03-25 23:29:14 +0100

    Inventory panel im Shop

---
+++ Scripts/Ui/Inventory.cs
@@ -0,0 +1,10 @@
+using Godot;
+
+namespace MonkeSurvivor.Scripts.Ui;
+
+public partial class Inventory : PanelContainer
+{
+    public override void _Ready() { }
+
+    public override void _Process(double delta) { }
+}
