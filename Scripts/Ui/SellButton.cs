ed2631bc917129ced5cf00446ce6b1d89a2a3c74
Christian Nytra <ch.nytra.13@gmail.com>
2024-03-26 00:01:55 +0100

    sellbuttonIcons dazu

--- Scripts/Ui/SellButton.cs
+++ Scripts/Ui/SellButton.cs
@@ -4,13 +4,7 @@ namespace MonkeSurvivor.Scripts.Ui;
 
 public partial class SellButton : PanelContainer
 {
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-	}
+    public override void _Ready() { }
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
-	}
+    public override void _Process(double delta) { }
 }
