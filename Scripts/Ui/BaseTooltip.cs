035c23c203775b520a219230cdf228df81fe40ca
Christian Nytra <ch.nytra.13@gmail.com>
2024-03-27 00:11:57 +0100

    basetooltip geklaut

---
+++ Scripts/Ui/BaseTooltip.cs
@@ -0,0 +1,39 @@
+﻿using Godot;
+using MonkeSurvivor.Scripts.Items;
+
+namespace MonkeSurvivor.Scripts.Ui;
+
+public abstract partial class BaseTooltip : PanelContainer
+{
+    public RichTextLabel Displayname  { get; set; }
+    public RichTextLabel Rarity       { get; set; }
+    public RichTextLabel Content      { get; set; }
+    public RichTextLabel Keywords     { get; set; }
+    public VBoxContainer Container    { get; set; }
+    public HBoxContainer Requirements { get; set; }
+    public RichTextLabel Effects      { get; set; }
+    public RichTextLabel Boni         { get; set; }
+    public RichTextLabel Fluff        { get; set; }
+    public Color         ColorRed     => new(0.86f, 0.09f, 0.09f);
+    public Color         ColorGreen   => new(0.02f, 0.7f, 0.08f);
+
+    public virtual void Show(BaseItem itemToShow)
+    {
+        if (itemToShow is null)
+            return;
+
+        Container    ??= GetNode<MarginContainer>("MarginContainer").GetNode<VBoxContainer>("VBoxContainer");
+        Requirements ??= Container.GetNode<HBoxContainer>("Requirements");
+        Effects      ??= Container.GetNode<RichTextLabel>("%Effects");
+        Boni         ??= Container.GetNode<RichTextLabel>("%Boni");
+        Content      ??= Container.GetNode<RichTextLabel>("%Content");
+        Fluff        ??= Container.GetNode<RichTextLabel>("Fluff");
+        Displayname  ??= Container.GetNode<RichTextLabel>("Name");
+        Rarity       ??= Container.GetNode<RichTextLabel>("Rarity");
+        Keywords     ??= Container.GetNode<RichTextLabel>("Keywords");
+
+        Visible = true;
+    }
+
+    public new virtual void Hide() => Visible = false;
+}
