4f074c143a1af58872cc439c480b017cce3faec4
Christian Nytra <ch.nytra.13@gmail.com>
2024-03-28 00:20:16 +0100

    itemgeneration im shop

--- Scenes/Shop.cs
+++ Scenes/Shop.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using System.Linq;
 using Godot;
 using MonkeSurvivor.Scripts.Ui;
 using MonkeSurvivor.Scripts.Utils;
@@ -6,15 +8,18 @@ namespace MonkeSurvivor.Scenes;
 
 public partial class Shop : Node
 {
-    private CharacterSheet characterSheet;
-    private Inventory      inventory;
-    private Label          moneyDisply;
-    private ShopPanel      shopPanel;
-    private PackedScene    BattleScene => ResourceLoader.Load<PackedScene>("res://Scenes/battle.tscn");
+    private          CharacterSheet characterSheet;
+    private          Inventory      inventory;
+    private readonly List<string>   itemScenes = new();
+    private          Label          moneyDisply;
+    private          ShopPanel      shopPanel;
+    private          PackedScene    BattleScene => ResourceLoader.Load<PackedScene>("res://Scenes/battle.tscn");
 
     public override void _Ready()
     {
         GetTree().Paused = false;
+
+        GenerateItems();
 
         shopPanel      = GetNode<ShopPanel>("%" + nameof(ShopPanel));
         inventory      = GetNode<Inventory>("%" + nameof(Inventory));
@@ -26,9 +31,33 @@ public partial class Shop : Node
         characterSheet.OnAttributeRaised += CharacterSheetOnOnAttributeRaised;
     }
 
-    public override void _Process(double delta) { }
-
     public void _on_button_pressed() => GetTree().ChangeSceneToPacked(BattleScene);
+
+    private void GenerateItems()
+    {
+        var itemDirectory = "res://Scenes/Items/";
+
+        using var directory = DirAccess.Open(itemDirectory);
+
+        if (directory is not null)
+        {
+            directory.ListDirBegin();
+            var fileName = directory.GetNext();
+
+            while (fileName != "")
+            {
+                if (directory.CurrentIsDir())
+                    return;
+
+                if (itemScenes.All(s => !s.Contains(fileName)))
+                    itemScenes.Add(itemDirectory + fileName);
+
+                fileName = directory.GetNext();
+            }
+        }
+        else
+            GD.Print($"An error occurred when trying to access the path. '{itemDirectory}'");
+    }
 
     private void CharacterSheetOnOnAttributeRaised(string attributename) { }
 }
