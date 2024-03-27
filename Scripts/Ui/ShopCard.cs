ddda690cf313654d458a8ad060e87763ec7ea492
Christian Nytra <ch.nytra.13@gmail.com>
2024-03-27 22:07:38 +0100

    shopcard mouseEvent

--- Scripts/Ui/ShopCard.cs
+++ Scripts/Ui/ShopCard.cs
@@ -4,9 +4,12 @@ namespace MonkeSurvivor.Scripts.Ui;
 
 public partial class ShopCard : PanelContainer
 {
-    private Label itemCostLabel;
-    private Label itemDescriptionLabel;
-    private Label itemNameLabel;
+    public delegate void MouseEventHandler(bool entered);
+
+    private Label                  itemCostLabel;
+    private Label                  itemDescriptionLabel;
+    private Label                  itemNameLabel;
+    public event MouseEventHandler OnMouseEvent;
 
     public override void _Ready()
     {
@@ -18,4 +21,8 @@ public partial class ShopCard : PanelContainer
     public override void _Process(double delta) { }
 
     public void _on_buy_pressed() => Modulate = new Color(Modulate, 0);
+
+    public void _on_mouse_entered_shopCard() => OnMouseEvent?.Invoke(true);
+
+    public void _on_mouse_exited_shopCard() => OnMouseEvent?.Invoke(false);
 }
