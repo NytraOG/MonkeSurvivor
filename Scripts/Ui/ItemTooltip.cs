eb0baa701db7375e19efc02fdcf2163e03945d2f
Christian Nytra <ch.nytra.13@gmail.com>
2024-03-27 20:42:40 +0100

    tooltip rein

---
+++ Scripts/Ui/ItemTooltip.cs
@@ -0,0 +1,8 @@
+namespace MonkeSurvivor.Scripts.Ui;
+
+public partial class ItemTooltip : BaseTooltip
+{
+    public override void _Ready() { }
+
+    public override void _Process(double delta) { }
+}
