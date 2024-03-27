6ef05bb8d4875d6ea1eb1caffb0ac2113b10f2f0
Christian Nytra <ch.nytra.13@gmail.com>
2024-03-28 00:06:21 +0100

    items rein!

--- Scripts/Items/BaseItem.cs
+++ Scripts/Items/BaseItem.cs
@@ -1,6 +1,12 @@
-﻿namespace MonkeSurvivor.Scripts.Items;
+﻿using Godot;
 
-public class BaseItem
+namespace MonkeSurvivor.Scripts.Items;
+
+public partial class BaseItem : Node2D
 {
-    
+    [Export]
+    public CompressedTexture2D ItemImage { get; set; }
+
+    [Export]
+    public int Price { get; set; }
 }
---
+++ Scripts/Items/Cheese.cs
@@ -0,0 +1,8 @@
+namespace MonkeSurvivor.Scripts.Items;
+
+public partial class Cheese : BaseItem
+{
+    public override void _Ready() { }
+
+    public override void _Process(double delta) { }
+}
