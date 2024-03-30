dc9b37cc00dd32bb89c6cc6511de1730a246a6a6
Christian Nytra <ch.nytra.13@gmail.com>
2024-03-30 20:49:53 +0100

    inventory generiert sich selbst slots

--- Scripts/Ui/Inventory.cs
+++ Scripts/Ui/Inventory.cs
@@ -4,7 +4,32 @@ namespace MonkeSurvivor.Scripts.Ui;
 
 public partial class Inventory : PanelContainer
 {
-    public override void _Ready() { }
+    private PackedScene inventorySlot;
 
-    public override void _Process(double delta) { }
+    private PackedScene InventorySlot
+    {
+        get
+        {
+            inventorySlot ??= ResourceLoader.Load<PackedScene>("res://Scenes/Ui/inventory_slot.tscn");
+            
+            return inventorySlot;
+        }
+    }
+
+    [Export]
+    public int SlotAmount { get; set; }
+    public override void _Ready()
+    {
+        var itemGrid = GetNode<GridContainer>("%ItemGrid");
+        
+        for (int i = 0; i < SlotAmount; i++)
+        {
+            var slot = InventorySlot.Instantiate<InventorySlot>();
+            itemGrid.AddChild(slot);
+        }
+    }
+
+    public override void _Process(double delta)
+    {
+    }
 }
