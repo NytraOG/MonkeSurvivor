9b8f9c0944c098af6c075395fbfb6037343835fd
Christian Nytra <ch.nytra.13@gmail.com>
2024-03-31 23:45:59 +0200

    Käse kann jetzt reggen

--- Scripts/Items/Banana.cs
+++ Scripts/Items/Banana.cs
@@ -2,18 +2,16 @@ namespace MonkeSurvivor.Scripts.Items;
 
 public partial class Banana : BaseItem
 {
-    public override void _Ready() { }
+    public override string GetTooltipDescription()
+    {
+        return "Mjam mjam, lecker Banane.";
+    }
 
-    public override void _Process(double delta) { }
-
-    public override string GetTooltipDescription() => "Mjam mjam, lecker Banane.";
     public override void ApplyEffectTo(Player player)
     {
-        
     }
 
     public override void DeductEffectFrom(Player player)
     {
-        
     }
 }
