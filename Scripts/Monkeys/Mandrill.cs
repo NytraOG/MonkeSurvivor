7f8c00558e4c27e746069bf7fb1c713e6ed26b97
Christian Nytra <ch.nytra.13@gmail.com>
2024-04-04 19:22:30 +0200

    formatting und mehr kleinere inventorySlots

--- Scripts/Monkeys/Mandrill.cs
+++ Scripts/Monkeys/Mandrill.cs
@@ -5,5 +5,4 @@ namespace MonkeSurvivor.Scripts.Monkeys;
 public partial class Mandrill : BaseMonkey
 {
     public override PackedScene StartingWeapon => ResourceLoader.Load<PackedScene>("res://Scenes/Weapons/coconut_grenade.tscn");
-
 }
