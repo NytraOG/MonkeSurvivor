8abaeaf7033d38c22d938623eeb1d1e6261a7538
Christian Nytra <ch.nytra.13@gmail.com>
2024-04-06 20:10:00 +0200

    bamboospear rein

---
+++ Scripts/Weapons/BambooSpear.cs
@@ -0,0 +1,16 @@
+namespace MonkeSurvivor.Scripts.Weapons;
+
+public partial class BambooSpear : BaseMeleeWeapon
+{
+    public override void _Ready()
+    {
+    }
+
+    public override void _Process(double delta)
+    {
+    }
+
+    protected override void ExecuteBehaviour()
+    {
+    }
+}
