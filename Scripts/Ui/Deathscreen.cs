058bcb1a9836929f3c93776c03870a20ec68363a
Christian Nytra <ch.nytra.13@gmail.com>
2024-04-28 17:14:57 +0200

    smooth deathscreen für transition zur characterselection

--- Scripts/Ui/Deathscreen.cs
+++ Scripts/Ui/Deathscreen.cs
@@ -1,15 +1,20 @@
 using Godot;
-using System;
+
+namespace MonkeSurvivor.Scripts.Ui;
 
 public partial class Deathscreen : PanelContainer
 {
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-	}
-
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	private PackedScene CharacterSeletionScene => ResourceLoader.Load<PackedScene>("res://Scenes/Ui/monkey_selection.tscn");
 	public override void _Process(double delta)
 	{
+		if (Input.IsAnythingPressed())
+			ResetPlaythrough();
+	}
+
+	private void ResetPlaythrough()
+	{
+		var tree = GetTree();
+		tree.ChangeSceneToPacked(CharacterSeletionScene);
+		tree.Paused = false;
 	}
 }
